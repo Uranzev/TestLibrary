fc2a19e299ef8c2b6809033255c3d7952998ab36
uranzev <uran_zew88@yahoo.com>
2018-09-20 19:37:17 +0800

    tri

--- mylibrary/src/main/java/com/example/mylibrary/ToasterMessage.kt
+++ mylibrary/src/main/java/com/example/mylibrary/ToasterMessage.kt
@@ -5,7 +5,7 @@ import android.widget.Toast
 @Suppress("unused")
 class ToasterMessage {
 
-    fun s(c: Context, message: String) {
+    fun toast(c: Context, message: String) {
 
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
 
