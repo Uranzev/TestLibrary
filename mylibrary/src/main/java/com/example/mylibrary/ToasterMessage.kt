469b05b9056012e5e9a56d5d34b176313291ba7a
uranzev <uran_zew88@yahoo.com>
2018-09-21 13:12:33 +0800

    check library version

--- mylibrary/src/main/java/com/example/mylibrary/ToasterMessage.kt
+++ mylibrary/src/main/java/com/example/mylibrary/ToasterMessage.kt
@@ -1,6 +1,7 @@
 package com.example.mylibrary
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 @Suppress("unused")
 object ToasterMessage {
@@ -9,5 +10,8 @@ object ToasterMessage {
 
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
 
+    }
+    fun hello(string:String){
+        Log.i("zev", string)
     }
 }
--- mylibrary/src/main/java/com/example/mylibrary/toast.kt
+++ mylibrary/src/main/java/com/example/mylibrary/toast.kt
@@ -2,9 +2,10 @@ package com.example.mylibrary
 
 import android.content.Context
 import android.widget.Toast
-
+    @Suppress("unused")
  fun toastMe(c: Context, message: String) {
 
     Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+
 
 }
